d44d51ede3bdde98baeeb6ad2ee8585f1f1a922f
Ricardo Vieira <a41726@alunos.ipb.pt>
2021-01-21 15:48:48 +0000

    Initialize project

---
+++ g24RicardoVieira41726_HenriqueAraujo41751/src/mycollections/LinkedDeque.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mycollections;
+
+import mycollections.linkedlists.DoublyLinkedList;
+
+/**
+ *
+ * @author paulo
+ */
+public class LinkedDeque<E> extends DoublyLinkedList<E> implements Deque<E>{
+    
+}
